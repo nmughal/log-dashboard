f2c8e559cba8b4be83723b250212861271639849
Sam Michelson <michelson_sam@bah.com>
2017-11-21 09:53:43 -0500

    Attempt to remove app controller
    Here are my changes so far looking into removing the app controller.

--- app/application/route.js
+++ app/application/route.js
@@ -1,17 +1,44 @@
 import Route from '@ember/routing/route';
+import { computed } from '@ember/object';
 
 export default Route.extend({
-    actions: {
-        triggerRefresh() {
-            this.refresh();
-        },
+    host: 'http://107.21.177.120:8080/stock-service',
+    previousTransition: null,
+    init() {
+        this.setHost(this.host);
+    },
 
+    setHost(host) {
+        let adapter = this.store.adapterFor('application');
+        adapter.set('host', host);
+    },
+    actions: {
         error (error, transition) {
-            let applicationController = this.controllerFor('application');
-            applicationController.set('previousTransition', transition);
+            // let applicationController = this.controllerFor('application');
+            // applicationController.set('previousTransition', transition);
 
             return true;
+        },
+
+        changeUrl(url) {
+            Ember.run.debounce(this, changeUrlValue, url, 1000);
         }
-    }
-            
+    },
+    fullUrl: computed('host', 'nameSpace', function() {
+        return `${this.get('host')}`;
+    })
 });
+
+function changeUrlValue(url) {
+    this.set('host', url);
+    this.setHost(url);
+    let routeName = this.currentRouteName;
+
+    if(!!this.previousTransition){
+        this.previousTransition.retry();
+        this.set('previousTransition', null);
+        return;
+    }
+
+    this.refresh();
+}
